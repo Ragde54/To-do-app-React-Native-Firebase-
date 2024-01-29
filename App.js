0692ac0415bd83061cb7bc8e609ca8bbbfb6b9c1
Ragde54 <e.feliu.54@gmail.com>
2024-01-29 13:31:00 +0100

    Update state and adding completing task

--- App.js
+++ App.js
@@ -16,23 +16,48 @@ import tempData from "./tempData";
 
 export default class App extends React.Component {
 	state = {
-    addTodoVisible: false
-  }
+		addTodoVisible: false,
+		lists: tempData,
+	};
 
-  toggleAddTodoModal() {
-    this.setState({addTodoVisible: !this.state.addTodoVisible})
-  }
+	toggleAddTodoModal() {
+		this.setState({ addTodoVisible: !this.state.addTodoVisible });
+	}
 
-  renderList = list => {
-    return <TodoList list={list}/>
-  }
+	renderList = (list) => {
+		return <TodoList list={list} updateList={this.updateList} />;
+	};
 
-  render() {
+	addList = (list) => {
+		this.setState({
+			lists: [
+				...this.state.lists,
+				{ ...list, id: this.state.length + 1, todos: [] },
+			],
+		});
+	};
+
+	updateList = (list) => {
+		this.setState({
+			lists: this.state.lists.map((item) => {
+				return item.id === list.id ? list : item;
+			}),
+		});
+	};
+
+	render() {
 		return (
 			<View style={styles.container}>
-        <Modal animationType="slide" visible={this.state.addTodoVisible} onRequestClose={() => this.toggleAddTodoModal()}>
-          <AddListModal closeModal={() => this.toggleAddTodoModal()}/>
-        </Modal>
+				<Modal
+					animationType="slide"
+					visible={this.state.addTodoVisible}
+					onRequestClose={() => this.toggleAddTodoModal()}
+				>
+					<AddListModal
+						closeModal={() => this.toggleAddTodoModal()}
+						addList={this.addList}
+					/>
+				</Modal>
 				<View style={{ flexDirection: "row" }}>
 					<View style={styles.divider} />
 					<Text style={styles.title}>
@@ -43,7 +68,10 @@ export default class App extends React.Component {
 				</View>
 
 				<View style={{ marginVertical: 48 }}>
-					<TouchableOpacity style={styles.addList} onPress={() => this.toggleAddTodoModal()}>
+					<TouchableOpacity
+						style={styles.addList}
+						onPress={() => this.toggleAddTodoModal()}
+					>
 						<AntDesign name="plus" size={24} color={colors.blue} />
 					</TouchableOpacity>
 
@@ -52,11 +80,12 @@ export default class App extends React.Component {
 
 				<View style={{ height: 275, paddingLeft: 32 }}>
 					<FlatList
-						data={tempData}
+						data={this.state.lists}
 						keyExtractor={(item) => item.name}
 						horizontal={true}
 						showsHorizontalScrollIndicator={false}
-            renderItem={({item}) => this.renderList(item)}
+						renderItem={({ item }) => this.renderList(item)}
+						keyboardShouldPersistTaps="always"
 					/>
 				</View>
 			</View>
